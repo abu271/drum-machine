50f13561443a78e5f01924fa1fd0506241418f1d
abuDarda97 <abudarda166@gmail.com>
2019-03-06 22:49:33 +0000

    drum-pad component added

--- src/container components/App/App.js
+++ src/container components/App/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Display from '../../presentational components/Display'
+import Display from '../../presentational components/Display/Display'
+import Pad from '../../presentational components/Pad/Pad'
 
 class App extends Component {
   render() {
     return (
       <div id="drum-machine" className="App">
-        <Display id="display" />
+        <div id="display">
+          <Display />
+        </div>
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
+        <Pad className="drum-pad" />
       </div>
     )
   }
---
+++ src/presentational components/Pad/Pad.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Pad = () => <div>PAD</div>
+
+export default Pad
