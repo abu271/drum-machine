5ec725003f338e0f69778856a73ce5a9a928af21
abuDarda97 <abudarda166@gmail.com>
2019-03-06 22:29:08 +0000

    display component added

--- src/container components/App/App.js
+++ src/container components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Display from '../../presentational components/Display'
 
 class App extends Component {
   render() {
     return (
       <div id="drum-machine" className="App">
+        <Display id="display" />
       </div>
     )
   }
---
+++ src/presentational components/Display.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Display = () => <p>hello</p>
+
+export default Display
