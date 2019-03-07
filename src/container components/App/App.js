0334d272f6dfcf977a48c892d92d1e3ec389e986
abuDarda97 <abudarda166@gmail.com>
2019-03-07 22:44:20 +0000

    new components added

--- src/container components/App/App.js
+++ src/container components/App/App.js
@@ -10,15 +10,24 @@ class App extends Component {
         <div id="display">
           <Display />
         </div>
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
-        <Pad className="drum-pad" />
+        <Pad sound="1" letter={"Q"}>
+        </Pad>
+        <Pad sound="2" letter={"W"}>
+        </Pad>
+        <Pad sound="3" letter={"E"}>
+        </Pad>
+        <Pad sound="4" letter={"A"}>
+        </Pad>
+        <Pad sound="5" letter={"S"}>
+        </Pad>
+        <Pad sound="6" letter={"D"}>
+        </Pad>
+        <Pad sound="7" letter={"Z"}>
+        </Pad>
+        <Pad sound="8" letter={"X"}>
+        </Pad>
+        <Pad sound="9" letter={"C"}>
+        </Pad>
       </div>
     )
   }
--- src/presentational components/Pad/Pad.js
+++ src/presentational components/Pad/Pad.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const Pad = () => <div>PAD</div>
+const Pad = (props) => {
+    return(
+        <div>
+            <button id={props.sound} className="drum-pad">
+                {props.letter}
+                <audio id={props.letter} className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+            </button>
+        </div>
+    )
+}
 
 export default Pad
